1a212870f8fb8692557f28728e1e5aba6adfe850
Steven Johnson <steve@xibalbastudios.com>
2015-04-10 23:25:50 -0500

    Brought over all building blocks; constants renamed to pi, noise modules in directory; grouped into constants and functions directories; core module to register all at once

---
+++ functions/decode_vars.lua
@@ -0,0 +1,48 @@
+--- Mixins to acquire data sent by @{corona_shader.encode.vars} routines.
+
+--
+-- Permission is hereby granted, free of charge, to any person obtaining
+-- a copy of this software and associated documentation files (the
+-- "Software"), to deal in the Software without restriction, including
+-- without limitation the rights to use, copy, modify, merge, publish,
+-- distribute, sublicense, and/or sell copies of the Software, and to
+-- permit persons to whom the Software is furnished to do so, subject to
+-- the following conditions:
+--
+-- The above copyright notice and this permission notice shall be
+-- included in all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+-- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+-- MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+-- IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+-- CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+-- SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+--
+-- [ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+--
+
+-- Export the functions.
+return {
+
+[[
+	vec2 UnitPair (float xy)
+	{
+		P_UV float axy = abs(xy);
+		P_UV float frac = fract(axy);
+
+		return vec2((axy - frac) / 1023., sign(xy) * frac + .5);
+	}
+]], [[
+	void UnitPair4_Out (vec4 xy, out vec4 x, out vec4 y)
+	{
+		P_UV vec4 axy = abs(xy);
+		P_UV vec4 frac = fract(axy);
+
+		x = (axy - frac) / 1023.;
+		y = sign(xy) * frac + .5;
+	}
+]]
+
+}
